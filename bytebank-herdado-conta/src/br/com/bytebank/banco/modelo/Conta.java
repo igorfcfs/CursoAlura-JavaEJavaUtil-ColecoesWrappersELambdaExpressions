035568674f8a897e19235a8c74535e3dde30d965
igorfcfs <igorfcfs@gmail.com>
2022-03-24 14:26:04 -0300

    Ordenacao de Listas

--- bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
@@ -6,7 +6,7 @@ package br.com.bytebank.banco.modelo;
  * @author Igor Casita
  *
  */
-public abstract class Conta {
+public abstract class Conta extends Object implements Comparable<Conta>{
 	protected double saldo;
 	private int agencia;
 	private int numero;
@@ -88,14 +88,19 @@ public abstract class Conta {
 	@Override
 	public boolean equals(Object ref) {
 		Conta outra = (Conta) ref;
-		if (this.agencia != outra.agencia && this.numero != outra.numero) {
+		if (this.agencia != outra.agencia || this.numero != outra.numero) {
 			return false;
 		}
 		return true;
 	}
+	
+	@Override
+	public int compareTo(Conta outra) {
+		return Double.compare(this.saldo, outra.saldo);
+	}
 
 	@Override
 	public String toString() {
-		return ", Numero: " + this.numero + ", Agencia: " + this.agencia;
+		return ", Numero: " + this.numero + ", Agencia: " + this.agencia + ", Saldo: " + this.saldo;
 	}
 }
--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/TesteArrayListEquals.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/TesteArrayListEquals.java
@@ -1,4 +1,4 @@
-package br.com.bytebank.banco.util;
+package br.com.bytebank.banco.test.util;
 
 import java.util.ArrayList;
 
